dfa5190b6337a6383266e1686dd0612ae9021931
dcaputi1 <53913604+dcaputi1@users.noreply.github.com>
2023-07-10 23:05:00 -0400

    update .clang-format
    - applied to diskbrowserdlg.cpp
    - fixed 'foreach' macro invocations
    - fixed short format switch

--- src/diskbrowserdlg.cpp
+++ src/diskbrowserdlg.cpp
@@ -4,25 +4,24 @@
  * Copyright 2023 D.Caputi
  */
 #include "diskbrowserdlg.h"
-#include "ui_diskbrowserdlg.h"
-#include "respeqtsettings.h"
+#include "diskimages/diskimage.h"
+#include "filesystems/ataridirentry.h"
 #include "filesystems/atarifilesystem.h"
 #include "filesystems/dos10filesystem.h"
 #include "filesystems/dos20filesystem.h"
 #include "filesystems/dos25filesystem.h"
 #include "filesystems/mydosfilesystem.h"
 #include "filesystems/spartadosfilesystem.h"
-#include "filesystems/ataridirentry.h"
-#include "diskimages/diskimage.h"
 #include "folderdisks.h"
 #include "mainwindow.h"
 #include "miscutils.h"
+#include "respeqtsettings.h"
+#include "ui_diskbrowserdlg.h"
 #include <QFileDialog>
 #include <QObject>
 
-DiskBrowserDlg::DiskBrowserDlg(SioWorkerPtr pSio, QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::DiskBrowserDlg)
+DiskBrowserDlg::DiskBrowserDlg(SioWorkerPtr pSio, QWidget *parent)
+    : QDialog(parent), ui(new Ui::DiskBrowserDlg)
 {
     sio = std::move(pSio);
 
@@ -37,7 +36,7 @@ DiskBrowserDlg::DiskBrowserDlg(SioWorkerPtr pSio, QWidget *parent) :
     connect(ui->cboFolderPath, SIGNAL(currentTextChanged(QString)), this, SLOT(onFolderChanged(QString)));
 
     refreshFoldersCombobox();
-    onFolderChanged("");        // reload the current item 0 in the combo
+    onFolderChanged("");// reload the current item 0 in the combo
 }
 
 DiskBrowserDlg::~DiskBrowserDlg()
@@ -60,7 +59,7 @@ void DiskBrowserDlg::clear()
 
 QString DiskBrowserDlg::getMostRecentFolder()
 {
-    QString   fileName = RespeqtSettings::instance()->mostRecentBrowserFolder();
+    QString fileName = RespeqtSettings::instance()->mostRecentBrowserFolder();
     QFileInfo fileInfo = QFileInfo(fileName);
 
     if (fileInfo.isFile())
@@ -71,7 +70,7 @@ QString DiskBrowserDlg::getMostRecentFolder()
 
 QString DiskBrowserDlg::getMostRecentDisk()
 {
-    QString   fileName = RespeqtSettings::instance()->mostRecentBrowserFolder();
+    QString fileName = RespeqtSettings::instance()->mostRecentBrowserFolder();
     QFileInfo fileInfo = QFileInfo(fileName);
 
     if (fileInfo.isFile())
@@ -116,14 +115,14 @@ void DiskBrowserDlg::onFolderChanged(QString folder)
 
     _folderDisks.load(folder);
 
-    clear();    // clear disk browser contents
+    clear();// clear disk browser contents
 
     // fill in any sub-directories
     auto folders = _folderDisks.folders();
-    foreach (const QString& subdir, folders)
+    foreach (const QString &subdir, folders)
     {
         auto item = new QTreeWidgetItem(ui->treeDisks);
-        auto icon = QIcon {":/icons/silk-icons/icons/folder_explore.png"};
+        auto icon = QIcon{":/icons/silk-icons/icons/folder_explore.png"};
         item->setText(0, subdir);
         item->setIcon(0, icon);
         setItemIsFolder(item, true);
@@ -131,10 +130,10 @@ void DiskBrowserDlg::onFolderChanged(QString folder)
 
     // fill in all disk image files
     auto disks = _folderDisks.disks();
-    foreach (const QString& disk, disks)
+    foreach (const QString &disk, disks)
     {
         auto item = new QTreeWidgetItem(ui->treeDisks);
-        auto icon = QIcon {":/icons/other-icons/floppy.png"};
+        auto icon = QIcon{":/icons/other-icons/floppy.png"};
         item->setText(0, disk);
         item->setIcon(0, icon);
         setItemIsFolder(item, false);
@@ -143,7 +142,7 @@ void DiskBrowserDlg::onFolderChanged(QString folder)
     if (disk.isEmpty())
     {
         // select the currently mounted disk if present in the list
-        auto    imageSet = RespeqtSettings::instance()->mountedImageSetting(0);
+        auto imageSet = RespeqtSettings::instance()->mountedImageSetting(0);
         QString pathName = imageSet.fileName;
 
         disk = QFileInfo(pathName).fileName();
@@ -152,7 +151,7 @@ void DiskBrowserDlg::onFolderChanged(QString folder)
     if (!disk.isEmpty() && disks.contains(disk))
     {
         auto items = ui->treeDisks->findItems(disk, Qt::MatchExactly);
-        QTreeWidgetItem* item = (items.length() > 0) ? items[0] : nullptr;
+        QTreeWidgetItem *item = (items.length() > 0) ? items[0] : nullptr;
         ui->treeDisks->setCurrentItem(item);
     }
 }
@@ -162,22 +161,22 @@ void DiskBrowserDlg::refreshFoldersCombobox()
     ui->cboFolderPath->blockSignals(true);
     ui->cboFolderPath->clear();
 
-    QStringList folders;    // build a list of MRU folders for the dropdown list
+    QStringList folders;// build a list of MRU folders for the dropdown list
 
     foreach (const QString& name, RespeqtSettings::instance()->recentBrowserFolders())
     {
         auto fileInf = QFileInfo(name);
         if (fileInf.exists())
         {
-            QString path = fileInf.isFile() ? fileInf.path() : name;    // don't want file names in dropdown
+            QString path = fileInf.isFile() ? fileInf.path() : name;// don't want file names in dropdown
             folders += path;
         }
-        else if (isDiskImage(name))  // MRU missing. First check if a disk is selected
+        else if (isDiskImage(name))// MRU missing. First check if a disk is selected
         {
             QString path = getParentDir(name);
             if (QFileInfo::exists(path))
             {
-                folders += path;    // Keep parent folder of bad disk
+                folders += path;// Keep parent folder of bad disk
             }
             else
             {
@@ -185,7 +184,7 @@ void DiskBrowserDlg::refreshFoldersCombobox()
                 RespeqtSettings::instance()->delMostRecentBrowserFolder(name);
             }
         }
-        else    // Simple case of missing folder
+        else// Simple case of missing folder
         {
             qDebug() << "!w" << tr("Disk Browser most recent list updated. Folder '%1' not found.").arg(name);
             RespeqtSettings::instance()->delMostRecentBrowserFolder(name);
@@ -224,14 +223,14 @@ void DiskBrowserDlg::itemSelectionChanged()
     RespeqtSettings::instance()->setMostRecentBrowserFolder(fullName);
     MainWindow::instance()->mountFileWithDefaultProtection(0, fullName);
 
-    QString fileList;   // get a list of files to show in the file list pane
+    QString fileList;// get a list of files to show in the file list pane
 
     if (sio)
     {
         const int deviceNo = 0;
-        auto img = qobject_cast <DiskImages::SimpleDiskImage*>(sio->getDevice(deviceNo + DISK_BASE_CDEVIC));
+        auto img = qobject_cast<DiskImages::SimpleDiskImage *>(sio->getDevice(deviceNo + DISK_BASE_CDEVIC));
         int fs = img->defaultFileSystem();
-        Filesystems::AtariFileSystem* atariFs = nullptr;
+        Filesystems::AtariFileSystem *atariFs = nullptr;
 
         switch (fs)
         {
@@ -245,8 +244,8 @@ void DiskBrowserDlg::itemSelectionChanged()
 
         if (atariFs)
         {
-            int     dir     = atariFs->rootDir();
-            auto    entries = atariFs->getEntries(dir);
+            int dir = atariFs->rootDir();
+            auto entries = atariFs->getEntries(dir);
 
             foreach (const Filesystems::AtariDirEntry& entry, entries)
                 fileList += entry.name() + "\n";
@@ -264,7 +263,7 @@ void DiskBrowserDlg::itemSelectionChanged()
         fileList = tr("!SIO device not available:\nNo Files");
     }
 
-    if (fileList[0] == '!') // detected error/annomally parsing filesystem above
+    if (fileList[0] == '!')// detected error/annomally parsing filesystem above
     {
         qDebug() << fileList;
         fileList[0] = '\n';
@@ -280,7 +279,7 @@ void DiskBrowserDlg::itemSelectionChanged()
     if (oldRatio != newRatio)
     {
         if (newRatio == 0.0)
-            newRatio = 1.0;     // TBD
+            newRatio = 1.0;// TBD
 
         ui->splitLeftAtrRightDirPng->setRatio(newRatio);
         ui->splitTopDirBotPng->setRatio(newRatio, true);
@@ -291,7 +290,7 @@ void DiskBrowserDlg::itemSelectionChanged()
 
 QString DiskBrowserDlg::getRecentDisk(QString folder)
 {
-    foreach (const QString& text, RespeqtSettings::instance()->recentBrowserFolders())
+    foreach (const QString &text, RespeqtSettings::instance()->recentBrowserFolders())
     {
         auto fi = QFileInfo(text);
         if (fi.isFile() && (fi.path() == folder))
@@ -310,9 +309,9 @@ QString DiskBrowserDlg::getParentDir(QString fileFolder)
     return fileFolder;
 }
 
-bool DiskBrowserDlg::isDiskImage(const QString& name)
+bool DiskBrowserDlg::isDiskImage(const QString &name)
 {
-    foreach (const QString& fileType, FileTypes::getDiskImageTypes())
+    foreach (const QString &fileType, FileTypes::getDiskImageTypes())
     {
         QString ext = fileType.right(4);
         if (name.endsWith(ext, osCaseSensitivity()))
@@ -320,7 +319,6 @@ bool DiskBrowserDlg::isDiskImage(const QString& name)
     }
     return false;
 }
-
 
 int DiskBrowserDlg::getHorzSplitPos()
 {
@@ -356,7 +354,7 @@ void DiskBrowserDlg::setVertSplitPos(int pos)
     ui->splitTopDirBotPng->setSizes(sizes);
 }
 
-void DiskBrowserDlg::itemDoubleClicked(QTreeWidgetItem* item, int)
+void DiskBrowserDlg::itemDoubleClicked(QTreeWidgetItem *item, int)
 {
     QString text = item->text(0);
     auto path = ui->cboFolderPath->currentText();
@@ -375,12 +373,12 @@ void DiskBrowserDlg::itemDoubleClicked(QTreeWidgetItem* item, int)
     }
 }
 
-void DiskBrowserDlg::setItemIsFolder(QTreeWidgetItem* item, bool isFolder)
+void DiskBrowserDlg::setItemIsFolder(QTreeWidgetItem *item, bool isFolder)
 {
     item->setData(0, Qt::UserRole, isFolder);
 }
 
-bool DiskBrowserDlg::itemIsFolder(QTreeWidgetItem* item)
+bool DiskBrowserDlg::itemIsFolder(QTreeWidgetItem *item)
 {
     return item->data(0, Qt::UserRole).toBool();
 }
