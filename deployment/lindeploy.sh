5125f86a967a9ebac59cc032848c122e1de97dea
Jochen Schäfer <josch1710@live.de>
2025-05-31 11:24:53 +0200

    - Fixing Linux üackage name

--- deployment/lindeploy.sh
+++ deployment/lindeploy.sh
@@ -18,4 +18,4 @@ cqtdeployer -bin "$1" -qmake "$2" $$QMAKE_QMAKE -libDir "$3" -icon "$3"/resource
     "$3"/atari/rcl_SpartaDos,"$3"/atari/rcl_RealDos,"$3"/atari/atari_8-bit_Menu\
     -description "RespeQt" -deployVersion "$4" \
     -tr "$3"/i18n/respeqt_de.qm,"$3"/i18n/respeqt_es.qm,"$3"/i18n/respeqt_pl.qm,"$3"/i18n/respeqt_ru.qm,"$3"/i18n/respeqt_sk.qm,"$3"/i18n/respeqt_tr.qm \
-    deb
+    deb -debOut "RespeQtLinux_$4$5.deb"
