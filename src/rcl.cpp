4f992b6b1c5f530b14a5a9b71bf7fa794d8dd5cf
Jochen <josch1710@live.de>
2026-08-27 10:33:46 +0200

    Github actions update Added missing files

--- src/rcl.cpp
+++ src/rcl.cpp
@@ -253,11 +253,7 @@ void RCl::handleCommand(const quint8 command, const quint8 aux1, const quint8 au
           int i, type;
           bool ok;
           i = imageFileName.lastIndexOf(".");
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
-          type = QStringView(imageFileName).mid(i + 1).toInt(&ok, 10);
-#else
           type = imageFileName.midRef(i + 1).toInt(&ok, 10);
-#endif
           if (ok && (type < 1 || type > 6)) ok = false;
           if (!ok) {
             qCritical() << "!e" << tr("[%1] Invalid image file attribute: %2").arg(deviceName()).arg(type);
